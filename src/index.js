6fbb2d2a884558bb6b319b2f19e80354ebbae118
Nguyen Hien <jaynguyen.vn99@gmail.com>
2025-05-09 02:29:26 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ app.engine(
         extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+   app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'resources\\views'));
 
 route(app);
 
